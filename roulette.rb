7d5d3c7cf78671750fd436a4fc40fd2ba8b9523d
Shawna Duvall <shawnaduvall@gmail.com@>
2020-10-19 19:11:29 -0700

    adds conditional for the 12's and for columns

--- roulette.rb
+++ roulette.rb
@@ -47,19 +47,50 @@ spins = num.to_i
 #track
 black_count = 0
 red_count = 0
+column_1_count = 0
+column_2_count = 0
+column_3_count = 0
+first_12 = 0
+second_12 = 0
+third_12 = 0
+
 
 spins.times do 
   rand_num = rand(0..37)
 p "#{rand_num}  #{double[rand_num]["color"]}"
+
   if double[rand_num]["color"] == "Red"
     red_count += 1
   elsif double[rand_num]["color"] == "Black"
     black_count += 1
   end
+
+  if double[rand_num]["column"] == 1
+    column_1_count += 1
+  elsif double[rand_num]["column"] == 2
+    column_2_count += 1
+  else double[rand_num]["column"] == 2
+    column_3_count +=1
+  end
+
+  if rand_num > 0 && rand_num <= 12
+    first_12 += 1
+  elsif rand_num > 12 && rand_num <= 24
+    second_12 += 1
+  else rand_num > 24 && rand_num <= 36
+    third_12 += 1
+  end
+
 end
 p "___RED OR BLACK___"
 p "Out of #{spins} spins #{red_count} of them were red."
 p "Out of #{spins} spins #{black_count} of them were black."
-
-
+p "___COLUMNS___"
+p "Out of #{spins} spins #{column_1_count} of them were in First/Left Column that starts with #1."
+p "Out of #{spins} spins #{column_2_count} of them were in Second/Center Column that starts with #2."
+p "Out of #{spins} spins #{column_3_count} of them were in Third/Left Column that starts with #3."
+p "___The 12's___"
+p "Out of #{spins} spins #{first_12} of them were in First 12."
+p "Out of #{spins} spins #{second_12} of them were in Second 12."
+p "Out of #{spins} spins #{third_12} of them were in Third 12."
 
